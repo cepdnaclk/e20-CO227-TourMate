1eef0f219231fc45ce6f83bd6f644cc9fd9b62fa
Tharindu Mapagedara <150885177+Tharindu09@users.noreply.github.com>
2024-09-13 11:00:37 +0530

    Revert "Add homepage front end"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,6 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
-import CurrencyConverter from "./pages/CurrencyConverter";
-import EmergencyConnector from "./pages/EmergencyConnector";
-import Weather from "./pages/Weather";
 import HotelPage from "./pages/HotelPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./utils/AuthContext";
@@ -25,7 +22,6 @@ import ForgotPassword from "./pages/ForgotPassword";
 const App = () => {
   return (
     <AuthProvider>
-
       <Router>
         <Box
           sx={{
@@ -55,16 +51,13 @@ const App = () => {
                   <Route path="/profile" element={<Profile />} />
                   <Route path="/add-bookmarks" element={<AddBookmarks />} />
                   <Route path="/create-plan" element={<Plan />} />
-                  <Route path="/find-hotel" element={<HotelPage />} />
-                  <Route path="/find-restaurant" element={<RestaurantPage />} />
                 </Route>
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/find-route" element={<RoutePage />} />
-                <Route path="/currency-converter" element={<CurrencyConverter />} />
-                <Route path="/emergency-connector" element={<EmergencyConnector />} />
-                <Route path="/weather" element={<Weather />} />
+                <Route path="/find-hotel" element={<HotelPage />} />
+                <Route path="/find-restaurant" element={<SearchPlace />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/navbar" element={<Navbar />} />
               </Routes>
